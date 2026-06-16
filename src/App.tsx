44666a3ce7c1fb9b3455797a56e4ea99a281f0e2
Istiak Ahmmed Bishal <167230922+bishal040@users.noreply.github.com>
2026-06-17 02:22:08 +0600

    Add GitHub Actions deployment workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { SmoothScroll } from './components/SmoothScroll';
 import { Navbar } from './components/Navbar';
 import { HomePage } from './components/pages/HomePage';
