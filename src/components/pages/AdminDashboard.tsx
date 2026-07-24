d377be5cd85bb7b83e49d464f018610e95993c4a
Istiak Ahmmed Bishal <167230922+bishal040@users.noreply.github.com>
2026-07-24 15:00:20 +0600

    Final update

--- src/components/pages/AdminDashboard.tsx
+++ src/components/pages/AdminDashboard.tsx
@@ -432,7 +432,7 @@ export const AdminDashboard: React.FC = () => {
             </div>
 
             {/* Scrollable Content */}
-            <div className="flex-1 overflow-y-auto p-6 space-y-8">
+            <div className="flex-1 overflow-y-auto p-6 space-y-8" data-lenis-prevent>
               
               {/* Top Row: Basic Info & Preferences */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
